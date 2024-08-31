4634bb2893816cb992a8ca76244eb022c768113f
Mridul Maikhuri <mridulmaikhuri1234@gmail.com>
2024-08-31 16:04:25 +0530

    header and home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 function Header() {
   return (
     <header className="flex justify-between items-center p-5 bg-white border-b border-black h-[9vh]">
-      <div className="font-bold text-2xl">ZKCare</div>
-      <nav>
+      <div className="font-bold text-2xl flex items-center gap-2">
+        <img src="logo.jpeg" alt="logo" width={50} height={50}/>
+        <p>ZKHealthCare</p>
+      </div>
+      <nav className='text-xl'>
         <ul className="flex space-x-8">
           <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Appointment</li>
           <li className="cursor-pointer">About Us</li>
         </ul>
       </nav>
-      <button className="bg-black text-white px-4 py-2 rounded-md">Login</button>
+      <button className="bg-black text-white px-4 py-2 rounded-md hover:opacity-70">Login</button>
     </header>
   )
 }
