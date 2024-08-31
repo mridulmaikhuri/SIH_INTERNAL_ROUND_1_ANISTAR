b6ef0f4c37e9603c74aa72774f0737b3a9c18d9f
Mridul Maikhuri <mridulmaikhuri1234@gmail.com>
2024-08-31 13:22:40 +0530

    remove .env

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
