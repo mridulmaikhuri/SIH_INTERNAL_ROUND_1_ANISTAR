2e2ec86795e3e69567aee908545ada44e97aba17
Mridul Maikhuri <mridulmaikhuri1234@gmail.com>
2024-08-31 19:42:10 +0530

    login and signup fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,14 +4,10 @@ import { useNavigate } from 'react-router-dom';
 function Header() {
   const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    navigate('../pages/SignUp/signup.jsx');
-  };
-  
   return (
     <header className="flex justify-between items-center p-3 bg-white border-b border-black h-[9vh] w-[100vw]">
       <div className="font-bold text-2xl flex items-center gap-2">
-        <img src="logo.jpeg" alt="logo" width={50} height={50}/>
+        <img src="logo.jpeg" alt="logo" width={50} height={50} />
         <p>ZKHealthCare</p>
       </div>
       <nav className='text-xl'>
@@ -20,7 +16,10 @@ function Header() {
           <li className="cursor-pointer">About Us</li>
         </ul>
       </nav>
-      <button onClick={handleButtonClick} className="bg-black text-white px-4 py-2 rounded-md hover:opacity-70">Login</button>
+      <div className='flex space-x-2'>
+        <button onClick={() => navigate('/login')} className="bg-black text-white px-4 py-2 rounded-md hover:opacity-70">Login</button>
+        <button onClick={() => navigate('/signup')} className="bg-black text-white px-4 py-2 rounded-md hover:opacity-70">Signup</button>
+      </div>
     </header>
   )
 }
