7df41e5ad05248369a78e2eaa7494b581a994974
Mridul Maikhuri <mridulmaikhuri1234@gmail.com>
2024-08-31 15:40:11 +0530

    normal commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="flex justify-between items-center p-5 bg-white border-b border-black h-[9vh]">
+      <div className="font-bold text-2xl">ZKCare</div>
+      <nav>
+        <ul className="flex space-x-8">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">Appointment</li>
+          <li className="cursor-pointer">About Us</li>
+        </ul>
+      </nav>
+      <button className="bg-black text-white px-4 py-2 rounded-md">Login</button>
+    </header>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'  
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Home from './pages/Home.jsx'
 
@@ -11,16 +11,16 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-        {
-            path: "/",
-            element: <Home />,
-        }
+      {
+        path: "/",
+        element: <Home />,
+      }
     ],
-},
+  },
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/> 
+    <RouterProvider router={router} />
   </StrictMode>,
 )
