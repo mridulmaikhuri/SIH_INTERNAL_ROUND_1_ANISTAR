f1665238717d204995115dda335cb67df7a58370
Himanshu Mallick <122736543+himanshumallickgit@users.noreply.github.com>
2024-09-01 16:40:14 +0530

    Update UserNavbar.jsx

--- frontend/src/components/UserNavbar.jsx
+++ frontend/src/components/UserNavbar.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const SideNavbar = () => {
-    const { userId } = useParams();
+  const { userId } = useParams();
 
   const patient = {
     name: 'John Doe',
-    image: '/user.png', // Replace with actual image URL
+    image: '/user.png',
   };
 
   return (
-    
     <div className="flex flex-col justify-between h-screen w-[20vw] bg-gray-800 text-white p-4">
       <div className="flex flex-col items-center mt-5">
+        
         <img
-          src={patient.image}
-          alt="Patient"
-          className="w-24 h-24 rounded-full mb-4"
+          src="/logo.jpeg"
+          alt="zkHealthCare"
+          className="w-28 h-auto mb-3"
         />
-        <h2 className="text-xl font-semibold">{patient.name}</h2>
+        <h1>ZK HEALTHCARE</h1>
       </div>
-
+      <br />
       <nav className="flex flex-col mt-16 space-y-4">
         <Link
           to={`/user/dashboard/${userId}`}
